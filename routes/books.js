7d0fba320b83ee37e177edf8c31b5b2049d314ab
TawshifTamjidAlam <tawshif27@gmail.com>
2019-07-01 21:26:30 -0700

    add route for detail, 404 and validation pending

--- routes/books.js
+++ routes/books.js
@@ -16,4 +16,30 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  try {
+    const books = await Book.findAll({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (books.length === 1) {
+      res.locals = {
+        book: books[0],
+        title: "Update Book",
+        headTitle: books[0].get("title"),
+        crudMethod: "put",
+        submitValue: "Update Book"
+      };
+      res.render("update-form");
+    } else {
+      res.redirect("/");
+    }
+  } catch (err) {
+    console.log(err);
+    res.end();
+  }
+});
+
 module.exports = router;
